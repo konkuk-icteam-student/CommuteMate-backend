5bf681f26a945617e89fad9e26d7c9d43efa70dc
simchaehyeon <tlacogussla@konkuk.ac.kr>
2025-11-14 10:32:46 +0900

    [FIX] 응답시 에러 코드 500으로만 반환되는 문제 해결

--- src/main/java/com/better/CommuteMate/global/exceptions/error/CategoryErrorCode.java
+++ src/main/java/com/better/CommuteMate/global/exceptions/error/CategoryErrorCode.java
@@ -2,17 +2,19 @@ package com.better.CommuteMate.global.exceptions.error;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
 public enum CategoryErrorCode implements CustomErrorCode {
 
-    CATEGORY_NOT_FOUND("존재하지 않는 categoryId입니다.", "[Error] : Category를 찾을 수 없습니다."),
-    CATEGORY_ALREADY_EXISTS("이미 존재하는 category 이름입니다.", "[Error] : Category 이름 중복 발생"),
-    CATEGORY_HAS_SUBCATEGORY("해당 category에 속한 subCategory가 있어 삭제할 수 없습니다.", "[Error] : Category 삭제 불가 - subCategory 존재");
+    CATEGORY_NOT_FOUND("존재하지 않는 categoryId입니다.", "[Error] : Category를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    CATEGORY_ALREADY_EXISTS("이미 존재하는 category 이름입니다.", "[Error] : Category 이름 중복 발생", HttpStatus.CONFLICT),
+    CATEGORY_HAS_SUBCATEGORY("해당 category에 속한 subCategory가 있어 삭제할 수 없습니다.", "[Error] : Category 삭제 불가 - subCategory 존재", HttpStatus.CONFLICT);
 
     private final String message;
     private final String logMessage;
+    private final HttpStatus status;
 
     @Override
     public String getName() {
--- src/main/java/com/better/CommuteMate/schedule/application/exceptions/ScheduleErrorCode.java
+++ src/main/java/com/better/CommuteMate/schedule/application/exceptions/ScheduleErrorCode.java
@@ -1,17 +1,20 @@
 package com.better.CommuteMate.schedule.application.exceptions;
 
 import com.better.CommuteMate.global.exceptions.error.CustomErrorCode;
+import org.springframework.http.HttpStatus;
 
 public enum ScheduleErrorCode implements CustomErrorCode {
-    SCHEDULE_PARTIAL_FAILURE("신청하신 일정 중 실패한 일정이 존재합니다.", "[Error] : 신청하신 일정 중 실패한 일정이 존재합니다."),
-    SCHEDULE_FAILURE("신청하신 일정이 모두 실패하였습니다.", "[Error] : 신청하신 일정이 모두 실패하였습니다.");
+    SCHEDULE_PARTIAL_FAILURE("신청하신 일정 중 실패한 일정이 존재합니다.", "[Error] : 신청하신 일정 중 실패한 일정이 존재합니다.", HttpStatus.MULTI_STATUS),
+    SCHEDULE_FAILURE("신청하신 일정이 모두 실패하였습니다.", "[Error] : 신청하신 일정이 모두 실패하였습니다.", HttpStatus.UNPROCESSABLE_ENTITY);
 
     private final String message;
     private final String logMessage;
+    private final HttpStatus status;
 
-    ScheduleErrorCode(String message, String logMessage) {
+    ScheduleErrorCode(String message, String logMessage, HttpStatus status) {
         this.message = message;
         this.logMessage = logMessage;
+        this.status = status;
     }
 
     @Override
@@ -27,5 +30,10 @@ public enum ScheduleErrorCode implements CustomErrorCode {
     @Override
     public String getMessage() {
         return this.message;
+    }
+
+    @Override
+    public HttpStatus getStatus() {
+        return null;
     }
 }
