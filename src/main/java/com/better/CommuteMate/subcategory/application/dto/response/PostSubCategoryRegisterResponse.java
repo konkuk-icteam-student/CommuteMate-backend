8f4fddb6d200b9f75a9ff81f84e85c3e8d2c894b
jiyun921 <ryn0921@naver.com>
2025-11-20 17:18:03 +0900

    [docs] subcategory 등록 dto 설명 추가

--- src/main/java/com/better/CommuteMate/subcategory/application/dto/response/PostSubCategoryRegisterResponse.java
+++ src/main/java/com/better/CommuteMate/subcategory/application/dto/response/PostSubCategoryRegisterResponse.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
+@Schema(description = "SubCategory 등록 응답 DTO")
 public class PostSubCategoryRegisterResponse extends ResponseDetail {
     @Schema(description = "등록된 소분류 ID")
     private final Long subCategoryId;
