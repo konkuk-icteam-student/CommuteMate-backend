d1b7491f61ae1bdb107f0725b3b6c10347b6b443
jiyun921 <ryn0921@naver.com>
2025-11-21 09:24:13 +0900

    [feat] SUBCATEGORY_NOT_FOUND 예외 구현

--- src/main/java/com/better/CommuteMate/global/exceptions/error/SubcategoryErrorCode.java
+++ src/main/java/com/better/CommuteMate/global/exceptions/error/SubcategoryErrorCode.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum SubcategoryErrorCode implements CustomErrorCode {
 
+    SUBCATEGORY_NOT_FOUND("존재하지 않는 subcategoryId입니다.", "[Error] : SubCategory를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
     SUBCATEGORY_ALREADY_EXISTS("이미 존재하는 subcategory 이름입니다.", "[Error] : SubCategory 이름 중복 발생", HttpStatus.CONFLICT),
     ;
 
