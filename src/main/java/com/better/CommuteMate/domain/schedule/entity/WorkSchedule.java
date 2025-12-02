b8604adf01893ee3b29ffc8cafb595282a5f5835
chabin37 <7chabin@gmail.com>
2025-12-02 17:28:49 +0900

    refactor: WorkSchedule 엔티티 개선 및 소프트 삭제 기능 추가
    - WorkScheduleStatusCode.java 삭제 (CodeType 사용으로 대체)
    - @Setter 제거하여 불변성 강화
    - isDeleted 필드 추가 (소프트 삭제 지원)
    - deleteApplySchedule() 메서드 추가 (일정 취소 처리)
    - approveChangeRequest() 메서드 추가 (관리자 변경 요청 승인)

--- src/main/java/com/better/CommuteMate/domain/schedule/entity/WorkSchedule.java
+++ src/main/java/com/better/CommuteMate/domain/schedule/entity/WorkSchedule.java
@@ -10,7 +10,6 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "work_schedule")
 @Getter
-@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
@@ -46,6 +45,32 @@ public class WorkSchedule {
 
     @Column(name = "updated_by")
     private Integer updatedBy;
+
+    @Column(name = "is_deleted", nullable = false)
+    @Builder.Default
+    private Boolean isDeleted = false;
+
+    // 스케줄 제거 요청 보낸 상태. statusCode는 최종적으로 [WS04: 일정 취소] 혹은 [WS01: 신청] 으로 저장됨.
+    public void deleteApplySchedule(Integer userId, CodeType codeType) {
+        this.statusCode = codeType;
+        this.updatedBy = userId;
+        if(codeType.equals(CodeType.WS02)) {// WS02: 승인(취소가 승인되었다는 뜻) -> WS04: 취소
+            this.isDeleted = true;
+            this.statusCode = CodeType.WS04;
+        }
+    }
+
+    // 변경 요청 승인 처리 (관리자용)
+    // typeCode로 넘어오는 값 : 요청 유형 코드 (CR01: 수정 요청, CR02: 삭제 요청)
+    public void approveChangeRequest(Integer adminId, CodeType typeCode) {
+        this.updatedBy = adminId;
+        if (typeCode.equals(CodeType.CR01)) { // CR01: 수정 요청
+            this.statusCode = CodeType.WS02; // WS02: 승인
+        } else if (typeCode.equals(CodeType.CR02)) { // CR02: 삭제 요청
+            this.statusCode = CodeType.WS04; // WS04: 취소
+            this.isDeleted = true;
+        }
+    }
 
     @PrePersist
     protected void onCreate() {
