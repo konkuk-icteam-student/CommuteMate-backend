880e172a479381c1e97167968b5f7eaefb5d41d2
simchaehyeon <tlacogussla@konkuk.ac.kr>
2026-01-06 15:20:25 +0900

    [FEAT] FAQ 수정 기능 개발 (테스트 전)

--- src/main/java/com/better/CommuteMate/faq/application/FaqService.java
+++ src/main/java/com/better/CommuteMate/faq/application/FaqService.java
@@ -5,7 +5,10 @@ import com.better.CommuteMate.domain.category.entity.SubCategory;
 import com.better.CommuteMate.domain.category.repository.CategoryRepository;
 import com.better.CommuteMate.domain.category.repository.SubCategoryRepository;
 import com.better.CommuteMate.domain.faq.dto.request.FaqCreateRequest;
+import com.better.CommuteMate.domain.faq.dto.request.FaqUpdateRequest;
 import com.better.CommuteMate.domain.faq.entity.Faq;
+import com.better.CommuteMate.domain.faq.entity.FaqHistory;
+import com.better.CommuteMate.domain.faq.repository.FaqHistoryRepository;
 import com.better.CommuteMate.domain.faq.repository.FaqRepository;
 import com.better.CommuteMate.domain.user.entity.User;
 import com.better.CommuteMate.domain.user.repository.UserRepository;
@@ -13,19 +16,24 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class FaqService {
 
     private final FaqRepository faqRepository;
+    private final FaqHistoryRepository faqHistoryRepository;
     private final CategoryRepository categoryRepository;
     private final SubCategoryRepository subCategoryRepository;
     private final UserRepository userRepository;
 
     public void createFaq(FaqCreateRequest request) {
+
+        // Todo 임시 인증 로직
         User writer = userRepository.findById(request.userId())
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다.")); // Todo 임시 로직
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
 
         Category category = categoryRepository.findByName(request.category())
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 대분류입니다."));
@@ -53,5 +61,47 @@ public class FaqService {
                 .build();
 
         faqRepository.save(faq);
+    }
+
+    public void updateFaq(Long faqId, FaqUpdateRequest request) {
+        Faq faq = faqRepository.findById(faqId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 FAQ입니다."));
+
+        // 해당 FAQ 삭제 여부 확인
+        if (Boolean.TRUE.equals(faq.getDeletedFlag())) {
+            throw new IllegalStateException("삭제된 FAQ는 수정할 수 없습니다.");
+        }
+
+        // 수정자 조회
+        // Todo 임시 인증 로직. 토큰 활용 안정화되면 삭제 예정
+        User editor = userRepository.findById(request.userId())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
+
+        // 히스토리 저장
+        FaqHistory history = FaqHistory.builder()
+                .faq(faq)
+                .title(faq.getTitle())
+                .category(faq.getSubCategory().getCategory().getName())
+                .subCategory(faq.getSubCategory().getName())
+                .content(faq.getContent())
+                .attachmentUrl(faq.getAttachmentUrl())
+                .manager(faq.getManager())
+                .writerName(faq.getWriterName())
+                .editorName(faq.getLastEditorName())
+                .editedAt(LocalDateTime.now())
+                .build();
+
+        faqHistoryRepository.save(history);
+
+        // FAQ 최신 상태로 업데이트
+        faq.setTitle(request.title());
+        faq.setContent(request.content());
+        faq.setEtc(request.etc());
+        faq.setAttachmentUrl(request.attachmentUrl());
+        faq.setManager(request.manager());
+
+        faq.setLastEditor(editor);
+        faq.setLastEditorName(request.editorName());
+        faq.setLastEditedAt(LocalDateTime.now());
     }
 }
--- src/main/java/com/better/CommuteMate/faq/controller/FaqController.java
+++ src/main/java/com/better/CommuteMate/faq/controller/FaqController.java
@@ -1,6 +1,7 @@
 package com.better.CommuteMate.faq.controller;
 
 import com.better.CommuteMate.domain.faq.dto.request.FaqCreateRequest;
+import com.better.CommuteMate.domain.faq.dto.request.FaqUpdateRequest;
 import com.better.CommuteMate.domain.faq.entity.Faq;
 import com.better.CommuteMate.faq.application.FaqService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -41,23 +42,32 @@ public class FaqController {
 
     @Operation(
             summary = "FAQ 수정",
-            description = "특정 FAQ를 수정하는 API입니다.\n" +
-                    "수정 시 기존 내용은 faq_history 테이블에 기록되고, faq 테이블은 최신 상태로 업데이트됩니다.\n" +
-                    "삭제된 FAQ는 수정이 불가합니다.."
+            description = """
+                    특정 FAQ를 수정하는 API입니다.
+                    수정 시 기존 내용은 faq_history 테이블에 기록되고,
+                    faq 테이블은 최신 상태로 업데이트됩니다.
+                    삭제된 FAQ(deleted_flag = true)는 수정할 수 없습니다.
+                    """
     )
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "FAQ 수정 성공"),
-            @ApiResponse(responseCode = "404", description = "FAQ를 찾을 수 없음", content = @Content),
-            @ApiResponse(responseCode = "400", description = "요청 데이터 오류", content = @Content)
+            @ApiResponse(responseCode = "400", description = "잘못된 요청"),
+            @ApiResponse(responseCode = "404", description = "FAQ를 찾을 수 없음"),
+            @ApiResponse(responseCode = "409", description = "삭제된 FAQ는 수정 불가")
     })
     @PutMapping("/{faqId}")
-    public ResponseEntity<?> updateFAQ(
-            @Parameter(description = "수정할 FAQ ID", required = true)
+    public ResponseEntity<Void> updateFaq(
+            @Parameter(
+                    description = "수정할 FAQ ID",
+                    required = true,
+                    example = "123"
+            )
             @PathVariable Long faqId,
-            @RequestBody Faq faq
+
+            @RequestBody FaqUpdateRequest request
     ) {
-        // TODO: FAQ 수정 로직 구현
-        return null;
+        faqService.updateFaq(faqId, request);
+        return ResponseEntity.ok().build();
     }
 
     @Operation(
