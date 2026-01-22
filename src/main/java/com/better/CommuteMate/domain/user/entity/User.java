faf13e7c4fb467a4f0a2615365ccb7b37c630b1a
jiyun921 <ryn0921@naver.com>
2026-01-22 13:39:52 +0900

    [fix] user 엔티티에서 manager 관련 개념 삭제

--- src/main/java/com/better/CommuteMate/domain/user/entity/User.java
+++ src/main/java/com/better/CommuteMate/domain/user/entity/User.java
@@ -1,13 +1,10 @@
 package com.better.CommuteMate.domain.user.entity;
 
-import com.better.CommuteMate.domain.category.entity.ManagerCategory;
 import com.better.CommuteMate.global.code.CodeType;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Table(name = "\"user\"", indexes = {
@@ -55,12 +52,7 @@ public class User {
     @Column(name = "refresh_token", length = 512)
     private String refreshToken;
 
-    // 이 user가 어떤 Categories를 담당하고 있는지 저장하는 리스트, manager role을 갖고 있는 경우만 가능
-    @OneToMany(mappedBy = "manager", cascade = CascadeType.ALL, orphanRemoval = true)
-    @Builder.Default
-    private List<ManagerCategory> managerCategories = new ArrayList<>();
-
-    @PrePersist
+        @PrePersist
     protected void onCreate() {
         createdAt = LocalDateTime.now();
         updatedAt = LocalDateTime.now();
