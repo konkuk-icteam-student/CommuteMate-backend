388cfadd6428567a74c0dc8dcb50c46e79117623
simchaehyeon <tlacogussla@konkuk.ac.kr>
2026-05-06 17:29:11 +0900

    [REFACTOR] request dto 내 필드 예외처리 notblank로 형식 통일

--- src/main/java/com/better/CommuteMate/category/application/dto/request/PostAICategoryRequest.java
+++ src/main/java/com/better/CommuteMate/category/application/dto/request/PostAICategoryRequest.java
@@ -1,10 +1,12 @@
 package com.better.CommuteMate.category.application.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 
 @Schema(description = "AI 카테고리 추천 요청 DTO")
 public record PostAICategoryRequest(
 
+        @NotBlank(message = "제목은 필수입니다.")
         @Schema(
                 description = "FAQ 제목",
                 example = "로그인이 안돼요",
