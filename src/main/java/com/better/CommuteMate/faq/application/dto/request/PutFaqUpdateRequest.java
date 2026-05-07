3960f259d43f097f11b765825c229710a9b32013
jiyun921 <ryn0921@naver.com>
2026-05-07 16:58:19 +0900

    [feat] faq 수정 시 이미지/파일도 수정하도록 추가

--- src/main/java/com/better/CommuteMate/faq/application/dto/request/PutFaqUpdateRequest.java
+++ src/main/java/com/better/CommuteMate/faq/application/dto/request/PutFaqUpdateRequest.java
@@ -3,6 +3,7 @@ package com.better.CommuteMate.faq.application.dto.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import java.util.List;
 
 @Schema(description = "FAQ 수정 요청 DTO")
 public record PutFaqUpdateRequest(
@@ -25,5 +26,11 @@ public record PutFaqUpdateRequest(
         Long categoryId,
 
         @Schema(description = "내용", example = "학정시 로그인을 하려는데 OTP 관련 메시지가 뜸")
-        String content
+        String content,
+
+        @Schema(description = "이미지 url 리스트")
+        List<String> imageUrls,
+
+        @Schema(description = "파일 url 리스트")
+        List<String> fileUrls
 ) {}
