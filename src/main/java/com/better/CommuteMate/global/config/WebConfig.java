aad44a3bb5e2206ed3ccbbfab8f9d3a77f784351
jiyun921 <ryn0921@naver.com>
2026-05-08 16:54:30 +0900

    [refactor] 환경변수 추가

--- src/main/java/com/better/CommuteMate/global/config/WebConfig.java
+++ src/main/java/com/better/CommuteMate/global/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.better.CommuteMate.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,9 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${storage.path}")
+    private String storagePath;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:/uploads/");
+                .addResourceLocations("file:" + storagePath + "/");
     }
 }
