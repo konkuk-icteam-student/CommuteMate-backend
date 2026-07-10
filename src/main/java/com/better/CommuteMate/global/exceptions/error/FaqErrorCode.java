aa2170c578f832f57f7342560b7c39ff8103de03
simchaehyeon <tlacogussla@konkuk.ac.kr>
2026-07-10 10:53:38 +0900

    [FEAT] faqErrorCode 추가

--- src/main/java/com/better/CommuteMate/global/exceptions/error/FaqErrorCode.java
+++ src/main/java/com/better/CommuteMate/global/exceptions/error/FaqErrorCode.java
@@ -13,7 +13,8 @@ public enum FaqErrorCode implements CustomErrorCode {
     INVALID_FAQ_HISTORY_DATE("해당 날짜의 FAQ 수정 이력이 존재하지 않습니다.", "[Error] : FAQ 히스토리 날짜 조회 실패", HttpStatus.BAD_REQUEST),
     CATEGORY_LIMIT_EXCEEDED("지정 가능한 카테고리 개수를 초과했습니다.", "[Error] : 카테고리 개수 초과", HttpStatus.BAD_REQUEST),
     CATEGORY_REQUIRED("카테고리가 지정되지 않았습니다.", "[Error] : 카테고리 지정 누락", HttpStatus.BAD_REQUEST),
-    RELATED_FAQ_NOT_FOUND("존재하지 않는 관련 FaqId입니다.", "[Error] : 관련 Faq를 찾을 수 없습니다.", HttpStatus.NOT_FOUND)
+    RELATED_FAQ_NOT_FOUND("존재하지 않는 관련 FaqId입니다.", "[Error] : 관련 Faq를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    INVALID_FAQ_STATUS("적합하지 않은 FAQ Status입니다.", "[Error] : FAQ status 부적합", HttpStatus.BAD_REQUEST)
     ;
 
     private final String message;
