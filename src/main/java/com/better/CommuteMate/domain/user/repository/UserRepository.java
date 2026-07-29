50e02216bd8548a8351e6fcfb853b35e788ed2bf
woo319 <jyw030319@konkuk.ac.kr>
2026-07-30 02:35:42 +0900

    feat: 사용자 검색 api 구현

--- src/main/java/com/better/CommuteMate/domain/user/repository/UserRepository.java
+++ src/main/java/com/better/CommuteMate/domain/user/repository/UserRepository.java
@@ -6,12 +6,20 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Optional<User> findByUserId(Long userId);
+    Optional<User> findByUserIdAndOrganizationId(Long userId, Long organizationId);
+
+    List<User> findAllByOrganizationIdAndRoleCodeAndNameContainingIgnoreCaseOrderByNameAscUserIdAsc(
+            Long organizationId,
+            CodeType roleCode,
+            String name
+    );
     Optional<User> findByUserIdAndOrganizationId(Long userId, Long organizationId);
     boolean existsByEmail(String email);
 
